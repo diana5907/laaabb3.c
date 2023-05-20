d443f94013d29f2522916879485000d1e91b5135
legion <legion@KAYANA>
2023-05-20 15:19:11 +0300

    Add project files.

---
+++ laaabb3.c/Task3.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int episodeNumber;
+
+    printf("Введіть номер серії: 2 ");
+    scanf_s("%d", &episodeNumber);
+
+    switch (episodeNumber) {
+    case 1:
+        printf("Серія 1: The Boy on the Bridge\n");
+        printf("Дата прем'єри: 22 січня 2018 року\n");
+        break;
+    case 2:
+        printf("Серія 2: A Fruitful Partnership\n");
+        printf("Дата прем'єри: 29 січня 2018 року\n");
+        break;
+    case 3:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 4:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 5:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 6:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 7:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 8:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 9:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    case 10:
+        printf("Серія 3: Silver Smile\n");
+        printf("Дата прем'єри: 5 лютого 2018 року\n");
+        break;
+    default:
+        printf("Невідомий номер серії\n");
+        break;
+    }
+
+    return 0;
+}
